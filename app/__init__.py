7d160556054e7bcde2f6c7f27049ba2938e0594e
kristiNezhaj <kristinezhaj@MacBook-Air-di-Kristi.local>
2024-07-12 17:45:05 +0200

    ultimo aggiornamento fron

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 
-db = SQLAlchemy()
-migrate = Migrate()
-login_manager = LoginManager()
-login_manager.login_view = 'login'
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+from app import routes, models
 
-    db.init_app(app)
-    migrate.init_app(app, db)
-    login_manager.init_app(app)
-
-    with app.app_context():
-        from app import routes, models
-
-    return app
-
-from app.models import User
-
-@login_manager.user_loader
+@login.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return models.User.query.get(int(user_id))
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
-from flask import render_template, flash, redirect, url_for, request, current_app
-from flask_login import current_user, login_user, logout_user
-from app import db
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
-from urllib.parse import urlparse, urljoin
 
-@current_app.route('/login', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    return "Hello, World!"
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -16,17 +21,24 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or urlparse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
-@current_app.route('/')
-def index():
-    return "Home Page"
-
-@current_app.route('/logout')
+@app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
