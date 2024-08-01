cbefdac74921321f1deed62c5ee282db6ed9f0cd
kristiNezhaj <kristinezhaj@Air-di-Kristi.homenet.telecomitalia.it>
2024-08-01 17:17:19 +0200

    routes

--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,15 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user, login_required
-from app import app, db
-from app.forms import LoginForm, RegistrationForm
-from app.models import User
+from flask import render_template
+from app import app
 
 @app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    return "Hello, World!"
+def home():
+    return render_template('TradeTrove/home.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/login')
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('TradeTrove/login.html')
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register')
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('TradeTrove/register.html')
