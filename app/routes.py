c91c518050006aa30446cf7032415b1b18b3516f
kristiNezhaj <kristinezhaj@Air-di-Kristi.homenet.telecomitalia.it>
2024-08-13 17:35:21 +0200

    ultima

--- app/routes.py
+++ app/routes.py
@@ -49,3 +49,62 @@ def init_routes(app):
             return redirect(url_for('home'))
 
         return render_template('register.html', form=form)
+    
+    @app.route('/categories')
+    def categories():
+        return render_template('categories.html')
+
+    @app.route('/offersbanner')
+    def offersbanner():
+        return render_template('offersbanner.html')
+
+    @app.route('/cart')
+    def cart():
+        return render_template('cart.html')
+    
+
+    @app.route('/contacts')
+    def contacts():
+        return render_template('contacts.html')
+    
+    @app.route('/faq')
+    def faq():
+        return render_template('faq.html')
+    
+    @app.route('/found')
+    def found():
+        return render_template('found.html')
+    
+    @app.route('/offers')
+    def offers():
+        return render_template('offers.html')
+    
+    
+    @app.route('/product')
+    def product():
+        return render_template('product.html')
+    
+    @app.route('/terms')
+    def terms():
+        return render_template('terms.html')
+    
+    @app.route('/question')
+    def question():
+        return render_template('question.html')
+    
+    @app.route('/profile')
+    def profile():
+        return render_template('profile.html')
+    
+    @app.route('/productreg')
+    def productreg():
+        return render_template('productreg.html')
+    
+    @app.route('/vendor')
+    def vendor():
+        return render_template('vendor.html')
+    
+    @app.route('/vendorprofile')
+    def vendorprofile():
+        return render_template('vendorprofile.html')
+    
