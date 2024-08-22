72c27fff47038f54e792d0ad826584126cf85fff
kristiNezhaj <kristinezhaj@Air-di-Kristi.homenet.telecomitalia.it>
2024-08-22 19:17:57 +0200

    registrazione venditore e login user

--- app/models.py
+++ app/models.py
@@ -10,9 +10,29 @@ class User(UserMixin, db.Model):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
-        
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Vendor(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(256))
+    piva = db.Column(db.String(20))
+    nome_azienda = db.Column(db.String(100))
+    indirizzo = db.Column(db.String(200))
+    città = db.Column(db.String(100))
+    nazione = db.Column(db.String(100))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<Vendor {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from flask import render_template, redirect, url_for, request, flash
-from app import db
-from app.models import User
-from app.forms import RegistrationForm, LoginForm
+from flask_login import login_user, logout_user, current_user, login_required
+from app import db, login_manager
+from app.models import User, Vendor
+from app.forms import RegistrationForm, LoginForm, VendorRegistrationForm
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 def init_routes(app):
     @app.route('/')
@@ -10,18 +15,27 @@ def init_routes(app):
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('home'))
         form = LoginForm()
         if form.validate_on_submit():
             username = form.username.data
             password = form.password.data
             user = User.query.filter_by(username=username).first()
             if user and user.check_password(password):
-                # Aggiungi la logica per la sessione utente qui
-                flash('Login successful')
+                login_user(user, remember=form.remember_me.data)
+                flash('Login successful', 'success')
                 return redirect(url_for('home'))
             else:
-                flash('Login failed. Check your username and/or password.')
+                flash('Login failed. Check your username and/or password.', 'danger')
         return render_template('login.html', form=form)
+
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        flash('You have been logged out.', 'info')
+        return redirect(url_for('home'))
 
     @app.route('/register', methods=['GET', 'POST'])
     def register():
@@ -31,7 +45,6 @@ def init_routes(app):
             email = form.email.data
             password = form.password.data
 
-            # Verifica se l'username o l'email esistono già
             if User.query.filter_by(username=username).first():
                 flash('Username already exists', 'danger')
                 return redirect(url_for('register'))
@@ -40,7 +53,6 @@ def init_routes(app):
                 flash('Email already registered', 'danger')
                 return redirect(url_for('register'))
 
-            # Crea un nuovo utente e salva nel database
             new_user = User(username=username, email=email)
             new_user.set_password(password)
             db.session.add(new_user)
@@ -53,7 +65,50 @@ def init_routes(app):
                 flash('An error occurred. Please try again.', 'danger')
 
         return render_template('register.html', form=form)
-    
+
+    @app.route('/vendor', methods=['GET', 'POST'])
+    def vendor():
+        form = VendorRegistrationForm()
+        if form.validate_on_submit():
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+            piva = form.piva.data
+            nome_azienda = form.nome_azienda.data
+            indirizzo = form.indirizzo.data
+            città = form.città.data
+            nazione = form.nazione.data
+            # Verifica se lo username o l'email sono già utilizzati da un utente
+            if User.query.filter_by(username=username).first():
+                flash('Username already exists in User accounts', 'danger')
+                return redirect(url_for('vendor'))
+
+            if User.query.filter_by(email=email).first():
+                flash('Email already registered in User accounts', 'danger')
+                return redirect(url_for('vendor'))
+            #utenti
+            if Vendor.query.filter_by(username=username).first():
+                flash('Username already exists', 'danger')
+                return redirect(url_for('vendor'))
+
+            if Vendor.query.filter_by(email=email).first():
+                flash('Email already registered', 'danger')
+                return redirect(url_for('vendor'))
+
+            new_vendor = Vendor(username=username, email=email, piva=piva, nome_azienda=nome_azienda, indirizzo=indirizzo, città=città, nazione=nazione)
+            new_vendor.set_password(password)
+            db.session.add(new_vendor)
+            try:
+                db.session.commit()
+                flash('Vendor registered successfully', 'success')
+                return redirect(url_for('home'))
+            except Exception as e:
+                db.session.rollback()
+                flash('An error occurred. Please try again.', 'danger')
+
+        return render_template('vendor.html', form=form)
+
+
     @app.route('/categories')
     def categories():
         return render_template('categories.html')
@@ -66,7 +121,6 @@ def init_routes(app):
     def cart():
         return render_template('cart.html')
     
-
     @app.route('/contacts')
     def contacts():
         return render_template('contacts.html')
@@ -82,7 +136,6 @@ def init_routes(app):
     @app.route('/offers')
     def offers():
         return render_template('offers.html')
-    
     
     @app.route('/product')
     def product():
@@ -104,11 +157,6 @@ def init_routes(app):
     def productreg():
         return render_template('productreg.html')
     
-    @app.route('/vendor')
-    def vendor():
-        return render_template('vendor.html')
-    
     @app.route('/vendorprofile')
     def vendorprofile():
         return render_template('vendorprofile.html')
-    
