5e8a1d2476739c0a96e7ce1700bf3f3f01791369
kristiNezhaj <kristinezhaj@MacBook-Air-di-Kristi.local>
2024-09-19 17:08:11 +0200

    delete product

--- app/models.py
+++ app/models.py
@@ -98,7 +98,7 @@ class OrderItem(db.Model):
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id',ondelete="CASCADE"), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     comment = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
